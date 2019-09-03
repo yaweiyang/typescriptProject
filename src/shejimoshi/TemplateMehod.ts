e414e223f786c6fd26054e8fa083957f5810c980
yang <hugehorse@sina.cn>
2019-09-03 19:06:37 +0800

    模板方法模式（未完）

---
+++ src/shejimoshi/TemplateMehod.ts
@@ -0,0 +1,12 @@
+abstract class TestPaper {
+    public firstQuestion(): void {
+        console.log("");
+    }
+    public secondQuestion(): void {
+
+    }
+    public thirdQuestion(): void {
+
+    }
+    abstract answer(): string;
+}
