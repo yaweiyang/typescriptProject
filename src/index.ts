8b216bc6ac6265772d9980a4491523605d3ff29f
yang <hugehorse@sina.cn>
2019-09-04 10:58:11 +0800

    模板方法模式

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@
 // import "./shejimoshi/Decorate";
 // import "./shejimoshi/AgentSystem";
 // import "./shejimoshi/Factory";
-import "./shejimoshi/Prototype";
+// import "./shejimoshi/Prototype";
+import "./shejimoshi/TemplateMehod";
 
 let hello = "hello Typescript";
 document.getElementsByClassName("app")[0].innerHTML = hello;
--- src/shejimoshi/TemplateMehod.ts
+++ src/shejimoshi/TemplateMehod.ts
@@ -1,12 +1,59 @@
 abstract class TestPaper {
     public firstQuestion(): void {
-        console.log("");
+        console.log("杨过得到，后来给了郭靖，练成倚天剑、屠龙刀的玄铁可能是（） \n a.球磨铸铁  b.马口铁  c.高速合金钢  d.碳素纤维");
+        console.log("答案为：" + this.firstAnswer());
     }
     public secondQuestion(): void {
-
+        console.log("杨过是帅哥吗？（） \n a.是  b.不是");
+        console.log("答案为：" + this.secondAnswer());
     }
     public thirdQuestion(): void {
-
+        console.log("小龙女是美女吗？ （） \n a.是  b.不是");
+        console.log("答案为：" + this.thirdAnswer());
     }
-    abstract answer(): string;
+    abstract firstAnswer(): string;
+    abstract secondAnswer(): string;
+    abstract thirdAnswer(): string;
 }
+class FristStudent extends TestPaper {
+    constructor() { super(); }
+    public firstAnswer(): string {
+        return "b";
+    }
+    public secondAnswer(): string {
+        return "a";
+    }
+    public thirdAnswer(): string {
+        return "a";
+    }
+}
+class SecondStudent extends TestPaper {
+    constructor() { super(); }
+    public firstAnswer(): string {
+        return "a";
+    }
+    public secondAnswer(): string {
+        return "b";
+    }
+    public thirdAnswer(): string {
+        return "a";
+    }
+}
+class ConsoleAction_TestPaper {
+    constructor() {
+        this.init();
+    }
+    private init(): void {
+        console.log("第一个学生的试卷");
+        let first: FristStudent = new FristStudent();
+        first.firstQuestion();
+        first.secondQuestion();
+        first.thirdQuestion();
+        console.log("第二个学生的试卷");
+        let second: SecondStudent = new SecondStudent();
+        second.firstQuestion();
+        second.secondQuestion();
+        second.thirdQuestion();
+    }
+}
+new ConsoleAction_TestPaper();
