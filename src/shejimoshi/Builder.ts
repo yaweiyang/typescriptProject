9ca634c8586f520baf2ab4363c3c48af83c7e458
yang <hugehorse@sina.cn>
2019-10-21 14:56:27 +0800

    尝试解决冲突

--- src/shejimoshi/Builder.ts
+++ src/shejimoshi/Builder.ts
@@ -1,22 +1,73 @@
-class Product {
-    parts: Array<string> = [];
-    // public
+// 建造者模式
+
+/**绘制一个人的抽象类 */
+abstract class PersonBuilder {
+    constructor() { }
+    /**绘制头部 */
+    public abstract buildHead(): void;
+    /**绘制身体 */
+    public abstract buildBody(): void;
+    /**绘制手 */
+    public abstract buildArm(): void;
+    /**绘制腿 */
+    public abstract buildLeg(): void;
+}
+class ThinPersonBuilder extends PersonBuilder {
     constructor() {
-        this.init();
+        super();
+        console.log("画一个瘦子");
     }
-    private init(): void {
-        let func = function (m: number, n: number): any {
-            if (m == 0) {
-                return n + 1;
-            } else if (m > 0 && n == 0) {
-                return func(m - 1, 1);
-            } else if (m > 0 && n > 0) {
-                return func(m - 1, func(m, n - 1));
-            } else {
-                return 0;
-            }
-        }
-        console.log("输出结果为：", func(3, 4));
+    public buildHead(): void {
+        console.log("绘制一个小脑袋");
+    }
+    public buildBody(): void {
+        console.log("绘制一个苗条的身体");
+    }
+    public buildArm(): void {
+        console.log("绘制纤细修长的手");
+    }
+    public buildLeg(): void {
+        console.log("绘制大长腿");
     }
 }
-new Product();
+class FatPersonBuilder extends PersonBuilder {
+    constructor() {
+        super();
+        console.log("画一个胖子");
+    }
+    public buildHead(): void {
+        console.log("绘制一个圆圆的脑袋");
+    }
+    public buildBody(): void {
+        console.log("绘制一个丰润的身体");
+    }
+    public buildArm(): void {
+        console.log("绘制短粗的手");
+    }
+    public buildLeg(): void {
+        console.log("绘制大象腿");
+    }
+}
+class PersonDirector {
+    private builder: PersonBuilder;
+    constructor(builder: PersonBuilder) {
+        this.builder = builder;
+    }
+    public createPerson(): void {
+        this.builder.buildHead();
+        this.builder.buildBody();
+        this.builder.buildArm();
+        this.builder.buildLeg();
+    }
+}
+class ConsoleAction_Builder {
+    constructor() { this.init(); }
+    private init() {
+        let director1: PersonDirector = new PersonDirector(new ThinPersonBuilder());
+        director1.createPerson();
+
+        let director2: PersonDirector = new PersonDirector(new FatPersonBuilder());
+        director2.createPerson();
+    }
+}
+new ConsoleAction_Builder();
